c1a1e92f33ffe24992c9ca9c1022b92d1da3ff83
bogdanberdnikov <bogdan.b@legalbet.com>
2019-07-19 15:36:05 +0600

    Update DataTable component

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,57 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
 import styles from './styles/DataTable.module.scss';
 
-const DataTable = ({ striped, bordered }) => {
+const DataTable = ({ striped, bordered, hovered, data }) => {
+    const [sort, setSort] = useState(null);
+
+    const sortingHandler = () => {
+        if (sort === 'asc' || sort === null) {
+            setSort('desc');
+        } else {
+            setSort('asc');
+        }
+    };
+
     return (
-        <table
-            className={classNames([
-                styles.dataTable,
-                striped && styles.striped,
-                bordered && styles.bordered
-            ])}
-        >
-            <thead>
-                <tr>
-                    <th>123</th>
-                    <th>456</th>
-                    <th>789</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>123</td>
-                    <td>456</td>
-                    <td>789</td>
-                </tr>
-                <tr>
-                    <td>123</td>
-                    <td>456</td>
-                    <td>789</td>
-                </tr>
-                <tr>
-                    <td>123</td>
-                    <td>456</td>
-                    <td>789</td>
-                    <td>000</td>
-                </tr>
-            </tbody>
-            <tfoot>
-            <tr>
-                <td>123</td>
-                <td>456</td>
-                <td>789</td>
-            </tr>
-            </tfoot>
-        </table>
+        <>
+            <table
+                className={classNames([
+                    styles.dataTable,
+                    striped && styles.striped,
+                    bordered && styles.bordered,
+                    hovered && styles.hovered
+                ])}
+            >
+                <thead>
+                    <tr>
+                        { data.cols.map(col => (
+                            <th
+                                key={col.field}
+                                className={classNames([
+                                    sort === 'asc' && styles.asc,
+                                    sort === 'desc' && styles.desc
+                                ])}
+                                onClick={sortingHandler}
+                            >{col.label}</th>
+                        )) }
+                    </tr>
+                </thead>
+                <tbody>
+                    { data.rows.map(row => (
+                        <tr key={row.id}>
+                            { data.cols.map(col => (
+                                <td key={col.label}>{row[col.field]}</td>
+                            )) }
+                        </tr>
+                    )) }
+                </tbody>
+            </table>
+        </>
     );
 };
 
 DataTable.propTypes = {
     striped: PropTypes.bool,
-    bordered: PropTypes.bool
+    bordered: PropTypes.bool,
+    data: PropTypes.shape({
+        cols: PropTypes.arrayOf(
+            PropTypes.shape({
+                label: PropTypes.string.isRequired,
+                field: PropTypes.string.isRequired,
+                sort: PropTypes.string,
+                width: PropTypes.number
+            })
+        ).isRequired,
+        rows: PropTypes.arrayOf(PropTypes.object).isRequired
+    })
 };
 
 DataTable.defaultProps = {
